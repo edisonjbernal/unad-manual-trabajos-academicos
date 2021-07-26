66c53f056eeb65e7c54212c911e3aa2c39aa3d68
Edison Johan Bernal M <edison@mejorvirtual.com>
2021-07-25 22:07:42 -0500

    Ajustes de manito inicial y estilo de fondo

--- stages/home/home.js
+++ stages/home/home.js
@@ -11,28 +11,28 @@ class stageHomeClass extends generalStage{
     content(){
         return `
         <h1>Iniciarán un recorrido por el Manual de Trabajos Académicos de la UNAD.
-        <strong>¡Bienvenidas y Bienvenidos!</strong>
-        </h1>
+  <strong>¡Bienvenidas y Bienvenidos!</strong>
+  </h1>
 
-        <div class="cursor-hand">
-            Manito
-        </div>
+  <div class="cursor-hand">
+      <img src="./images/hand-down.png" alt="Manito apuntado abajo">
+  </div>
 
-        <div class="introduction-select">
-            <div class="introduction-select-p-1">
-                <p>Navegue por una breve introducción previo al viaje hacia el recurso</p>
-            </div>
-            <div class="introduction-select-button-1">
-                <button class="btn-general" onclick="stage.get('introduction')">Introducción</button>
-            </div>
-            <div class="introduction-select-p-2">
-                <p>Comience su viaje aquí  y avance por las diferentes y avance por las rutas haciendo clic sobre las estaciones que encontrará</p>
-            </div>
-            
-            <div class="introduction-select-button-2">
-                <button class="btn-general" onclick="stage.get('stationMap')">Comenzar</button>
-            </div>
-        </div>
+  <div class="introduction-select">
+      <div class="introduction-select-p-1">
+          <p>Navegue por una breve introducción previo al viaje hacia el recurso</p>
+      </div>
+      <div class="introduction-select-button-1">
+          <button class="btn-general" onclick="stage.get('introduction')">Introducción</button>
+      </div>
+      <div class="introduction-select-p-2">
+          <p>Comience su viaje aquí  y avance por las diferentes y avance por las rutas haciendo clic sobre las estaciones que encontrará</p>
+      </div>
+      
+      <div class="introduction-select-button-2">
+          <button class="btn-general" onclick="stage.get('stationMap')">Comenzar</button>
+      </div>
+  </div>
         `;
     }
     importCssFiles(){
