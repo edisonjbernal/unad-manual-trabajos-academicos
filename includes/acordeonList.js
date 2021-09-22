c0257f9274e5c96a58027301a4992ed8c8359ee7
Edison Johan Bernal M <edison@mejorvirtual.com>
2021-09-22 10:58:33 -0500

    Eliminación de comentarios accodeon list

--- includes/acordeonList.js
+++ includes/acordeonList.js
@@ -1,8 +1,7 @@
 class acordeonListClass{
     show(element,noScrollIntoView = null){
-        /* element.style.display='none'; */
+   
         let acordeonContainer = element.parentElement;
-
         let showStatus=acordeonContainer.getAttribute('data-show');
         if(showStatus==1){
             acordeonContainer.setAttribute('data-show','');
@@ -12,12 +11,5 @@ class acordeonListClass{
             acordeonContainer.setAttribute('data-show','1');
             acordeonContainer.classList.add('show');
         }
-        /* console.log('MOSTRAR : popUpToDiscoverClass');
-        console.log(acordeonContainer); */
-        
-        /* if(!noScrollIntoView){
-            textContainer.scrollIntoView({ block: 'center', behavior: 'smooth' });
-        } */
-        
     }
 }
