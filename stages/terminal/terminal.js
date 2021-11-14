0842bba3b4b5f662c115b3b1c8a0bb556a92db22
willtarazona <53130553+willtarazona@users.noreply.github.com>
2021-11-14 17:31:57 -0500

    Update terminal.js
    Acompaña a Julián en conocer POR a conocer

--- stages/terminal/terminal.js
+++ stages/terminal/terminal.js
@@ -20,7 +20,7 @@ class terminalClass extends generalStage{
       <p><strong>Marcela</strong></p>
       <p>Marcela es una estudiante de pregrado de la Universidad que requiere conocer con urgencia los lineamientos generales para la presentación de trabajos escritos dentro de nuestra institución. Con ella podrás familiarizarte con aspectos esenciales como el tipo y tamaño de letra, el interlineado, la portada, entre otros.</p>
       <p><strong>Julián</strong></p>
-      <p>Es un estudiante de pregrado de nuestra institución que recientemente tuvo un inconveniente en la presentación de uno de sus trabajos por incurrir involuntariamente en plagio. Acompaña a Julián en conocer la manera apropiada de referenciar y citar en trabajos escritos académicos a través de las normas APA, y a entender con claridad de qué se trata el plagio.</p>
+      <p>Es un estudiante de pregrado de nuestra institución que recientemente tuvo un inconveniente en la presentación de uno de sus trabajos por incurrir involuntariamente en plagio. Acompaña a Julián a conocer la manera apropiada de referenciar y citar en trabajos escritos académicos a través de las normas APA, y a entender con claridad de qué se trata el plagio.</p>
       </div>
       <div class="text-center">
       <button class="btn-general" onclick="stage.get('terminalContent')">Ingresar</button>
