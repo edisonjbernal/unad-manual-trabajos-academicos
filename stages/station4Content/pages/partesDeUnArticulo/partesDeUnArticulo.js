48f3131571d5935527776b98400e8558890bcc46
willtarazona <53130553+willtarazona@users.noreply.github.com>
2021-11-15 23:59:24 -0500

    Update partesDeUnArticulo.js

--- stages/station4Content/pages/partesDeUnArticulo/partesDeUnArticulo.js
+++ stages/station4Content/pages/partesDeUnArticulo/partesDeUnArticulo.js
@@ -14,11 +14,11 @@ class station4ContentClass_partesDeUnArticulo extends generalPage{
         <div class="start">
             <h3>Partes de un artículo de investigación</h3>
             <p>Los artículos de investigación suelen publicarse en revistas científicas especializadas en tu
-              área de conocimiento y desempeño profesional; sin embargo ocasionalmente también
+              área de conocimiento y desempeño profesional; sin embargo, ocasionalmente también
               pueden ser parte de los documentos de memorias de eventos académicos (congresos,
               seminarios, simposios), o incluso ser publicados como capítulos de libros científicos. De
               aquí la relevancia de tener muy en claro qué elementos de tu proyecto investigativo serán
-              incluidos y presentados dentro de un artículo, que en últimas v iene fungiendo como un
+              incluidos y presentados dentro de un artículo, que en últimas viene fungiendo como un
               documento corto proveniente de tu proyecto original que tiene como fin principal dar a
               conocer tu trabajo dentro de la comunidad académica global.
               </p>
@@ -42,7 +42,7 @@ class station4ContentClass_partesDeUnArticulo extends generalPage{
                               Your browser does not support the audio tag.
                             </audio>
                             <p><strong>Título</strong></p>
-                            <p>El título debe dar una perspectiva completa de tu proyecto, y será tu estrategia inicial de invitar a los lectores a acercarse al artículo en sí. El máximo de palabras de un título es generalmente de nueve (9), pero esto puede variar dependiendo de los lineamientos de cada revista o fuente de publicación. Las palabras deben ser claras para todo tipo de lectores, y en lo posible tener un sinónimo claro para cualquier país, es decir evitar palabras o expresiones que sean comprensibles para una región o país en particular.</p>
+                            <p>El título debe dar una perspectiva completa de tu proyecto, y será tu estrategia inicial de invitar a los lectores a acercarse al artículo en sí. El máximo de palabras de un título es generalmente de nueve (9), pero esto puede variar dependiendo de los lineamientos de cada revista o fuente de publicación. Las palabras deben ser claras para todo tipo de lectores, y en lo posible tener un sinónimo claro para cualquier país, es decir, evitar palabras o expresiones que sean comprensibles para una región o país en particular.</p>
                         </div>
                     </div>
                 </div>
@@ -63,7 +63,7 @@ class station4ContentClass_partesDeUnArticulo extends generalPage{
                           Your browser does not support the audio tag.
                         </audio>
                             <p><strong>Autores</strong></p>
-                            <p>En este aparte se presenta un nombre y un apellido de quien/es realizaron la investigación; además de la institución académica o científica a la que pertenece, información breve de su formación académica y laboral, y su correo electrónico.</p>
+                            <p>En este aparte se presenta un nombre y un apellido de quien/es realizaron la investigación; además, de la institución académica o científica a la que pertenece, información breve de su formación académica y laboral, y su correo electrónico institucional.</p>
                             
                         </div>
                     </div>
@@ -85,7 +85,7 @@ class station4ContentClass_partesDeUnArticulo extends generalPage{
                           Your browser does not support the audio tag.
                         </audio>
                         <p><strong>Resumen</strong></p>
-                        <p>Es una parte esencial de las publicaciones, y quizás la más importante al tratarse de una publicación en una revista especializada, pues este aparte es el único que es visible en las bases de datos que fungen como motores de búsqueda para las comunidades académicas, y que son leídas por quienes están en pesquisa de información bibliográfica útil para sus fines académicos e investigativos. La clave del resumen es generar interés por la lectura y la información completa del artículo.</p>
+                        <p>Es una parte esencial de las publicaciones, y quizás la más importante al tratarse de una publicación en una revista especializada, pues este aparte es el único que es visible en las bases de datos que fungen como motores de búsqueda para las comunidades académicas. Además, son leídas por quienes están en pesquisa de información bibliográfica útil para sus fines académicos e investigativos. La clave del resumen es generar interés por la lectura y la información completa del artículo.</p>
                         </div>
                     </div>
                 </div>
